0374d3bed7fc644334055b26e7be2c587214b570
Snape 479 <Severus_Snape93@protonmail.com>
2021-07-13 16:32:24 -0700

    use other llvm

--- build_static_win32.bat
+++ build_static_win32.bat
@@ -1,8 +1,12 @@
 cargo clean
 
-set OPENSSL_LIB_DIR=C:\Program Files (x86)\OpenSSL-Win32/lib/
-set OPENSSL_INCLUDE_DIR=C:\Program Files (x86)\OpenSSL-Win32/include
+git clone https://github.com/bitcoinmw/win32_helpers
+bzip2 -dc win32_helpers/llvm_openssl_win32.tar.bz2 | tar xvf -
+
+rem We want to trigger ssl static for all rust projects.
+set LIBCLANG_PATH=%cd%\LLVM_OpenSSL-Win32
+set OPENSSL_LIB_DIR=%cd%\LLVM_OpenSSL-Win32\lib\
+set OPENSSL_INCLUDE_DIR=%cd%\LLVM_OpenSSL-Win32\include\
 set OPENSSL_STATIC=yes
-set LIBCLANG_PATH=C:\Program Files (x86)\LLVM\bin\
 
 call .ci\win32_cargo.bat +stable-i686-pc-windows-msvc build --release
